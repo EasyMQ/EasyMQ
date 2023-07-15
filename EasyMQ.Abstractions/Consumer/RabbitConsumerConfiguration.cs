e43453aa608834e13a3e4a31ca53fa2c2bafd1e5
Sharthak Ghosh <sharthak.ghosh@live.in>
2023-07-15 19:27:50 +0530

    Add basic config for retriable consumers

--- EasyMQ.Abstractions/Consumer/RabbitConsumerConfiguration.cs
+++ EasyMQ.Abstractions/Consumer/RabbitConsumerConfiguration.cs
@@ -22,15 +22,43 @@ public class RabbitConsumerConfiguration
     public bool QueueAutoDelete { get; set; }
     public bool ExchangeAutoDelete { get; set; }
     public string RoutingKey { get; set; } = string.Empty;
+    public RetryPolicy RetryPolicy { get; set; }
 
     public void Validate()
     {
         Guard.IsNotNullOrEmpty(EventName);
         Guard.IsNotNullOrEmpty(EventHandlerName);
         Guard.IsNotNullOrEmpty(QueueName);
+        if (RetryPolicy is not null)
+        {
+            Guard.IsNotNull(RetryPolicy.RetryType);
+        }
     }
 }
 public class Binding
 {
     public Dictionary<string, dynamic> Arguments { get; set; }
+}
+
+public class RetryPolicy
+{
+    public RetryType? RetryType;
+    public int RetryNumberOfTimes { get; set; }
+    
+}
+
+public enum RetryType
+{
+    Immediate,
+    Delayed
+}
+
+public class Backoff
+{
+    public BackoffType Type;
+}
+
+public enum BackoffType
+{
+    Fib
 }
---
+++ EasyMQ.Consumer/ResilientEventConsumer.cs
@@ -0,0 +1,49 @@
+﻿using EasyMQ.Abstractions;
+using EasyMQ.Abstractions.Consumer;
+using Microsoft.Extensions.Options;
+
+namespace EasyMQ.Consumers;
+
+public sealed class ResilientEventConsumer<TEvent, THandler> : IEventConsumer
+    where TEvent : class, IEvent, new()
+    where THandler : IEventHandler<TEvent>
+{
+    private Func<IEventHandler<TEvent>> _handlerFactory;
+    private IOptions<List<RabbitConsumerConfiguration>> _consumerConfiguration;
+
+    public ResilientEventConsumer(Func<IEventHandler<TEvent>> handlerFactory,
+        IOptions<List<RabbitConsumerConfiguration>> consumerConfiguration)
+    {
+        _handlerFactory = handlerFactory;
+        _consumerConfiguration = consumerConfiguration;
+    }
+    public Task ConsumeAsync(ReceiverContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ConsumerConfiguration GetConsumerConfiguration()
+    {
+        var rabbitConfig = _consumerConfiguration.Value;
+        rabbitConfig.ForEach(c => c.Validate());
+
+        var config = rabbitConfig.FirstOrDefault(
+            c =>
+                c.EventName.Equals(typeof(TEvent).Name) && c.EventHandlerName.Equals(typeof(THandler).Name));
+        var bindings = config.Bindings.Select(configBinding => configBinding.Arguments).ToList();
+        return new ConsumerConfiguration()
+        {
+            QueueName = config.QueueName,
+            ExchangeName = config.ExchangeName,
+            ExchangeType = config.ExchangeType,
+            Bindings = bindings,
+            IsDurable = config.IsDurable,
+            RoutingKey = config.RoutingKey,
+            IsExclusiveQueue = config.IsExclusiveQueue,
+            QueueAutoDelete = config.QueueAutoDelete,
+            ExchangeAutoDelete = config.ExchangeAutoDelete,
+            ShouldDeclareExchange = config.ShouldDeclareExchange,
+            ShouldDeclareQueue = config.ShouldDeclareQueue
+        };
+    }
+}
