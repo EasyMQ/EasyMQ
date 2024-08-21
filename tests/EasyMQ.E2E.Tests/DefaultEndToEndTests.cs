1e23f35174c350747350330c50d90835f8e6dca2
coderookie1994 <sharthak.ghosh@live.in>
2024-08-22 04:15:31 +0530

    Added support for default exchange

---
+++ tests/EasyMQ.E2E.Tests/DefaultEndToEndTests.cs
@@ -0,0 +1,51 @@
+﻿
+using EasyMQ.Abstractions.Producer;
+using EasyMQ.Consumer;
+using EasyMQ.Consumers;
+using EasyMQ.E2E.Tests.TestHandlers;
+using EasyMQ.Extensions.DependencyInjection;
+using EasyMQ.Publisher;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EasyMQ.E2E.Tests
+{
+    public class DefaultEndToEndTests: Fixture
+    {
+        private IFakeLogger _fakeLogger;
+
+        protected override void AddServices(IServiceCollection services, IConfigurationRoot configurationRoot)
+        {
+            _fakeLogger = Substitute.For<IFakeLogger>();
+            services
+                .AddEventConsumer<DefaultEvent, DefaultEventHandler>()
+                .AddEventProducer<DefaultEvent>()
+                .AddSingleton(_fakeLogger)
+                .AddTransient(sp => Substitute.For<ILogger<AsyncEventPublisher<DefaultEvent>>>())
+                .AddTransient(sp => Substitute.For<ILogger<ConsumerEventHost<AsyncEventConsumer<DefaultEvent, DefaultEventHandler>>>>());
+
+            base.AddServices(services, configurationRoot);
+        }
+
+        [Fact]
+        public async Task ForADefaultExchange_MessagePublishedWithTheRoutingKey_ShouldEndUpInTheQueue()
+        {
+            await Given<IEventPublisher<DefaultEvent>>(i =>
+            {
+                return i.PublishAsync(new DefaultEvent { EventName = "default-event" }, new ProducerContext());
+            });
+            Thread.Sleep(400);
+
+            await Then<IFakeLogger>(i =>
+            {
+                i.Received(1).Log(Arg.Any<string>());
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
---
+++ tests/EasyMQ.E2E.Tests/TestHandlers/DefaultEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using EasyMQ.Abstractions;
+using EasyMQ.Abstractions.Consumer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMQ.E2E.Tests.TestHandlers
+{
+    public class DefaultEvent: IEvent
+    {
+        public string EventName { get; set; }
+    }
+    public class DefaultEventHandler : IEventHandler<DefaultEvent>
+    {
+        private IFakeLogger _logger;
+
+        public DefaultEventHandler(IFakeLogger fakeLogger)
+        {
+            _logger = fakeLogger;
+        }
+        public Task Handle(ReceiverContext receiverContext, DefaultEvent @event)
+        {
+            _logger.Log(@event.EventName);
+            return Task.CompletedTask;
+        }
+    }
+}
